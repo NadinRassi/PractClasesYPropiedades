8fdd941aab7f46ed952296c6c7a25f63147b636a
NadinRassi <sofianadinrassi@gmail.com>
2023-01-31 22:20:07 -0300

    entitys

---
+++ WindowsApp/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProducto = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnProducto
+            // 
+            this.btnProducto.Location = new System.Drawing.Point(139, 107);
+            this.btnProducto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnProducto.Name = "btnProducto";
+            this.btnProducto.Size = new System.Drawing.Size(109, 25);
+            this.btnProducto.TabIndex = 0;
+            this.btnProducto.Text = "Producto";
+            this.btnProducto.UseVisualStyleBackColor = true;
+            this.btnProducto.Click += new System.EventHandler(this.btnProducto_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 107);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ClienteIndividuo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Plum;
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnProducto);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProducto;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsApp/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsApp.Entidades;
+
+namespace WindowsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProducto_Click(object sender, EventArgs e)
+        {
+            Producto producto = new Producto();
+
+
+            producto.Nombre = "Pan";
+            producto.Descripcion = "Bolsa de Pan";
+            producto.PrecioCosto = 2000;
+            producto.Margen = 0.15;
+            producto.IVA = 0.21;
+            producto.Proveedor = "Carlos Perez";
+            producto.Categoria = "Comestibles";
+            producto.SubCategoria = "Panificados";
+
+            //GET
+
+            MessageBox.Show("Nombre: " + producto.Nombre + "\n" + "Descripcion: " + producto.Descripcion + "\n" + "Precio: " + producto.PrecioCosto.ToString() + "\n" + "Margen: " + producto.Margen.ToString() + "\n" + "Iva: " + producto.IVA.ToString() + "\n" + "Precio bruto: " + producto.PrecioBruto + "\n" + "Precio Venta: " + producto.PrecioVenta + "\n" + "Proveedor: " + producto.Proveedor + "\n" + "Categoria: " + producto.Categoria + "\n" + "Subcategoria: " + producto.SubCategoria
+                );
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ClienteIndividuo1 clienteindividuo = new ClienteIndividuo1();
+            clienteindividuo.Nombre = "Juan";
+            clienteindividuo.Apellido = "Lopez";
+            clienteindividuo.CUIT = "24324853940";
+            clienteindividuo.Email = "juanl09@gmail.com";
+            clienteindividuo.Telefono = "1184753219";
+            clienteindividuo.Direccion = "Lopez de Vega 123";
+
+            MessageBox.Show("Nombre: " + clienteindividuo.Nombre + "\n" + "Apellido: " + clienteindividuo.Apellido + "\n" + "CUIT: " + clienteindividuo.CUIT + "\n" + "Email: " + clienteindividuo.Email + "\n" + "Teléfono: " + clienteindividuo.Telefono + "\n" + "Dirección: " + clienteindividuo.Direccion);
+        }
+    }
+
+}
+    
+
